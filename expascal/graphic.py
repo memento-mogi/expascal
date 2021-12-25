092ca506a2196ee4485a6da76cfcb0213ecd7664
maetaka-2020248 <maetaka-2020248@penguin>
2021-12-25 13:38:27 +0900

    forth dayo

--- expascal/graphic.py
+++ expascal/graphic.py
@@ -1,41 +1,41 @@
+import os
+import glob
+import re
 import tkinter as tk
 from tkinter import ttk
 import objective
 import compare as comp
 
-root = tk.Tk()
-root.title("拡張パスカルの三角形")
-root.geometry("1000x800")
-
-#table setting
-table = ttk.Treeview(root)
-table["columns"] = (0,1,2,3)
-table.column(0,width=75)
-table.column(1,width=75)
-table.column(2,width=100)
-table.column(3,width=125)
-table.heading(0, text="No.")
-table.heading(1, text="size")
-table.heading(2, text="devide")
-table.heading(3, text="01")
-table.place(x=100, y=200)
-
 pascal_list = []
 index = -1
-z_o_flag = tk.BooleanVar()
+
+TYPENO_BIN = 0
+TYPENO_IMG = 1
+
+def list_init():
+    pascal_list = []
+    for tri_bin in glob.glob("../tri_bin/*"):
+        args = re.findall(r"\d+", tri_bin)
+        pascal_list.append(args)
+    print(pascal_list)
 
 def create():
     global index
     global z_o_flag
     devide = int(devide_inbox.get())
     size = int(size_inbox.get())
+
     pascal = objective.Pascal(devide, size)
     pascal_list.append(pascal)
     index = index + 1
-    pascal.set_body(pascal.set_axis())
-    print(z_o_flag.get())
+
+    if not pascal.exist_check(TYPENO_BIN):
+        pascal.set_body(pascal.set_axis())
+        print("new create")
+
     if z_o_flag.get():
         pascal.toZeroone()
+
     table.insert("","end",values=(index, size, devide, z_o_flag.get()))
     return
 
@@ -53,15 +53,24 @@ def compare():
     num_a = int(no1_inbox.get())
     num_b = int(no2_inbox.get())
     if pascal_list[num_a].size == pascal_list[num_b].size:
-        comp.comp(pascal_list[num_a], pascal_list[num_b])
+        print(comp.comp(pascal_list[num_a], pascal_list[num_b]))
     else:
         print("同じサイズで比較して下さい")
     return
 
+# tk setting
+root = tk.Tk()
+root.title("拡張パスカルの三角形")
+root.geometry("1000x800")
+
+# checkbox var
+z_o_flag = tk.BooleanVar()
+
+# styles
 label_s = ttk.Style().configure("TLabel", foreground="black", background="white", font=("",30))
 button_s = ttk.Style().configure("TButton", foreground="red", background="gray", font=("",30))
 
-#status input
+# status input boxes
 ttk.Label(text="size").grid(row=0, column=0)
 size_inbox = ttk.Entry(width=15, font=("", 20))
 size_inbox.grid(row=0, column=1)
@@ -71,7 +80,7 @@ devide_inbox.grid(row=1, column=1)
 zeroone_check = ttk.Checkbutton(onvalue=1, offvalue=0, variable=z_o_flag, text="01only")
 zeroone_check.grid(row=2, column=0)
 
-#number input
+# indexnumber input boxes
 ttk.Label(text="no1").grid(row=0, column=2)
 no1_inbox = ttk.Entry(width=15, font=("", 20))
 no1_inbox.grid(row=0, column=3)
@@ -79,9 +88,24 @@ ttk.Label(text="no2").grid(row=1, column=2)
 no2_inbox = ttk.Entry(width=15, font=("", 20))
 no2_inbox.grid(row=1, column=3)
 
+# buttons
 ttk.Button(root, text="create", command=create).grid(row=2, column=1)
 ttk.Button(root, text="save", command=save).grid(row=2, column=2)
 ttk.Button(root, text="count", command=count).grid(row=2, column=3)
 ttk.Button(root, text="compare", command=compare).grid(row=2, column=4)
 
+# table
+table = ttk.Treeview(root)
+table["columns"] = (0,1,2,3)
+table.column(0,width=75)
+table.column(1,width=75)
+table.column(2,width=100)
+table.column(3,width=125)
+table.heading(0, text="No.")
+table.heading(1, text="size")
+table.heading(2, text="devide")
+table.heading(3, text="01")
+table.place(x=100, y=200)
+
+list_init()
 root.mainloop()
--- expascal/objective.py
+++ expascal/objective.py
@@ -2,14 +2,22 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 import subprocess
+import pickle
 
 np.set_printoptions(threshold=np.inf)
+imgpath = os.environ.get("EXPASCAL_IMG")
+binpath = os.environ.get("EXPASCAL_BIN")
 
-class Pascal:    
-    def __init__(self, devide, size):
+TYPENO_BIN = 0
+TYPENO_IMG = 1
+
+class Pascal:
+    def __init__(self, devide, size, z_o_flag):
         self.devide: int = devide
         self.size: int = size
-
+        self.z_o_flag: int = z_o_flag
+        self.paths_full: tuple = (f"{binpath}{self.devide}-{self.size}-{self.z_o_flag}tri.bin",
+                                  f"{imgpath}{self.devide}-{self.size}-{self.z_o_flag}tri.png")
 
     def set_axis(self) -> np.matrix:
         np.zeros(self.size)
@@ -17,13 +25,12 @@ class Pascal:
         for column_axis in range (2, self.size):
             thenum = (axis[0][column_axis-1]+axis[0][column_axis-2]) % self.devide
             axis[0].append(thenum)
-        
-        temp_body = np.zeros((self.size-1,self.size))
+
+        temp_body = np.zeros((self.size-1, self.size))
         axis_1 = np.concatenate((axis, temp_body))
-        axis_1 = np.mat(axis_1, dtype="int16")
+        axis_1 = np.mat(axis_1, dtype="int32")
         axis_2 = axis_1.T
         return(axis_1 + axis_2)
-
 
     def set_body(self, triangle):
         for row in range(1, self.size):
@@ -31,11 +38,23 @@ class Pascal:
                 thenum = (triangle[row-1, column] + triangle[row, column-1]) % self.devide
                 triangle[row, column] = thenum
         triangle[0,0] = 1
-        self.contents = triangle
+        with open(f"{self.paths_full[TYPENO_BIN]}", mode="wb") as fi:
+            pickle.dump(triangle, fi)
         return
 
+    def toZeroone(self, triangle):
+        triangle = np.where(self.contents==0, 0, 1)
+        return
+
+    def exist_check(self, file_type) -> bool:
+        return os.path.exists(f"{self.paths_full[file_type]}")
+
+    def contents(self):
+        with open(f"{self.paths_full[TYPENO_BIN]}", mode="rb") as fi:
+            return pickle.load(fi)
+
     def create_image(self):
-        filename = os.environ.get("EXPASCAL_IMG") + str(self.devide)+"-"+str(self.size)+"img.png"
+        filename = f"{imgpath}{str(self.devide)}-{str(self.size)}img.png"
         fig = plt.figure(dpi=1024)
         ax = fig.add_subplot(1,1,1)
         ax.imshow(self.contents, cmap="Blues")
@@ -48,12 +67,10 @@ class Pascal:
         print(isZero,"/",self.size**2)
         return
 
-    def toZeroone(self):
-        self.contents = np.where(self.contents==0, 0, 1)
+
 
 if __name__ == "__main__":
-    pascal1 = Pascal(4, 34)
+    pascal1 = Pascal(7, 34, 1)
     pascal1.set_body(pascal1.set_axis())
-    print(pascal1.contents)
-    pascal1.create_image()
-
+    print(pascal1.contents())
+    print(pascal1.exist_check(TYPENO_BIN))
